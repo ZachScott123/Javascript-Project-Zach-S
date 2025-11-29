ac056c62d33382540ef513628def75773de520aa
Zach Scott <codzapman@gmail.com>
2025-11-29 15:05:34 -0330

    COMMIT 1 - Files created and basic code developed.

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const getElement = selector => document.querySelector(selector);
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const answer = getElement("#answer_input");
+
+    getElement("#submit").addEventListener("click", () => {
+        
+    });
+
+});
