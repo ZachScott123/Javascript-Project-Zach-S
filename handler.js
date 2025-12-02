5758b5eb4abd7e951f3a8cffab09cfea7ae1af12
Zach Scott <codzapman@gmail.com>
2025-12-01 23:00:56 -0330

    COMMIT 7 - Now checks whether a question has been asked and pushes to a correct list, which then displays correct questions at the end. Also added more questions.

--- handler.js
+++ handler.js
@@ -4,10 +4,18 @@
 const getElement = selector => document.querySelector(selector);
 
 export function loadQuestion(data) {
-    data.currentQuestion = Math.floor(Math.random() * data.questions.length);
-    data.questionOutput.textContent = data.questions[data.currentQuestion];
+    let index
+    do {
+        index = Math.floor(Math.random() * data.questions.length);
+    } while (data.usedQuestions.includes(index) && data.usedQuestions.length < data.questions.length);
+
+    data.currentQuestion = index;
+    data.questionOutput.textContent = data.questions[index];
+
     data.answerInput.value = "";
     data.answerInput.focus();
+
+    data.usedQuestions.push(index);
 };
 
 export function answerCheck(data) {
@@ -15,43 +23,68 @@ export function answerCheck(data) {
 
     if (userAnswer == "" || data.answerCount >= 10) return; 
 
+    let isCorrect = false;
+
     switch (data.currentQuestion) {
         case 0:
-            if (userAnswer == "ford model t") data.correctCount++; break;
+            if (userAnswer == "ford model t") isCorrect = true; break;
         case 1:
-            if (userAnswer == "volvo") data.correctCount++; break;
+            if (userAnswer == "volvo") isCorrect = true; break;
         case 2:
-            if (userAnswer == "sport utility vehicle") data.correctCount++; break;
+            if (userAnswer == "sport utility vehicle") isCorrect = true; break;
         case 3:
-            if (userAnswer == "all terrain vehicle") data.correctCount++; break;
+            if (userAnswer == "all terrain vehicle") isCorrect = true; break;
         case 4:
-            if (userAnswer == "91") data.correctCount++; break;
+            if (userAnswer == "91") isCorrect = true; break;
         case 5:
-            if (userAnswer == "1909") data.correctCount++; break;
+            if (userAnswer == "1909") isCorrect = true; break;
         case 6:
-            if (userAnswer == "lamborghini") data.correctCount++; break;
+            if (userAnswer == "lamborghini") isCorrect = true; break;
         case 7:
-            if (userAnswer == "honda" || userAnswer == "toyota") data.correctCount++; break;
+            if (userAnswer == "honda" || userAnswer == "toyota") isCorrect = true; break;
         case 8:
-            if (userAnswer == "1832") data.correctCount++; break;
+            if (userAnswer == "1832") isCorrect = true; break;
         case 9:
-            if (userAnswer == "corolla") data.correctCount++; break;
+            if (userAnswer == "corolla") isCorrect = true; break;
         case 10:
-            if (userAnswer == "1902") data.correctCount++; break;
+            if (userAnswer == "1902") isCorrect = true; break;
         case 11:
-            if (userAnswer == "henry ford") data.correctCount++; break;
+            if (userAnswer == "henry ford") isCorrect = true; break;
         case 12:
-            if (userAnswer == "3") data.correctCount++; break;
+            if (userAnswer == "3") isCorrect = true; break;
         case 13:
-            if (userAnswer == "audi") data.correctCount++; break;
+            if (userAnswer == "audi") isCorrect = true; break;
         case 14:
-            if (userAnswer == "low") data.correctCount++; break;
+            if (userAnswer == "low") isCorrect = true; break;
         case 15:
-            if (userAnswer == "bmw") data.correctCount++; break;
+            if (userAnswer == "mini") isCorrect = true; break;
         case 16:
-            if (userAnswer == "enzo ferrari" || userAnswer == "enzo") data.correctCount++; break;
+            if (userAnswer == "enzo ferrari" || userAnswer == "enzo") isCorrect = true; break;
         case 17:
-            if (userAnswer == "tractor") data.correctCount++; break;
+            if (userAnswer == "tractor") isCorrect = true; break;
+        case 18:
+            if (userAnswer == "ferrari") isCorrect = true; break;
+        case 19:
+            if (userAnswer == "mustang") isCorrect = true; break;
+        case 20:
+            if (userAnswer == "kia" || userAnswer == "hyundai") isCorrect = true; break;
+        case 21:
+            if (userAnswer == "bentley" || userAnswer == "rolls royce") isCorrect = true; break;
+        case 22:
+            if (userAnswer == "porsche 911" || userAnswer == "911") isCorrect = true; break;
+        case 23:
+            if (userAnswer == "nascar") isCorrect = true; break;
+        case 24:
+            if (userAnswer == "f1" || userAnswer == "formula 1") isCorrect = true; break;
+        case 25:
+            if (userAnswer == "mitsubishi") isCorrect = true; break;
+        case 26:
+            if (userAnswer == "subaru") isCorrect = true; break;
+    }
+
+    if (isCorrect) {
+        data.correctCount++
+        data.correctQuestions.push(data.questions[data.currentQuestion]);
     }
 
     data.answerCount++
@@ -61,9 +94,25 @@ export function answerCheck(data) {
 
     if (data.answerCount >= 10) {
         data.questionOutput.textContent = `Congratulations, you got ${data.correctCount} questions correct!`
-        getElement("#answer-submit").disabled = true;
+
         data.answerInput.value = "";
         data.answerInput.disabled = true;
+        data.answerSubmit.disabled = true;
+
+        const correctDiv = document.createElement("div");
+        correctDiv.id = "correct-questions";
+        correctDiv.innerHTML = "<h2>Questions that were correctly answered:</h2>";
+        
+        const ul = document.createElement("ul");
+        data.correctQuestions.forEach(index => {
+            const li = document.createElement("li")
+            li.textContent = index;
+            ul.appendChild(li);
+        });
+
+        correctDiv.appendChild(ul);
+        getElement("#game-box").appendChild(correctDiv);
+
     } else{
         data.loadQuestion();
     }
--- main.js
+++ main.js
@@ -11,25 +11,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const data = {
     questions: [ "What was the first mass-produced automobile?", //Ford Model T
-                        "Which car company invented the modern-day seatbelt?", //Volvo
-                        "What does SUV stand for?", //Sport Utility Vehicle
-                        "What does ATV stand for?", //All Terrain Vehicle
-                        "What is the highest-octane commercially available gas type in NL?", //91
-                        "When was the first Front-Wheel-Drive car built?", //1909
-                        "Which was the first car company to manufacture a 'Super Car'?", //Lamborghini
-                        "Which mainstream car company is primarily known for their reliability?", //Honda or Toyota
-                        "When was the first electric vehicle produced?", //1832
-                        "What is the best selling car model in the world?", //Corolla
-                        "When was the first speeding ticket issued?", //1902
-                        "Who invented the term 'Tune-Up' as in to tune up your car?", //Henry Ford
-                        "How many total car models contain factory cd-players now?", //3
-                        "Which car brand was the first to incorporate LED running lights?", //Audi
-                        "In an automatice vehicle, your drive selectors include P-R-N-D-L, what does the L stand for?", //Low
-                        "Which car brand's logo is based on a pair of propellers?", //BMW
-                        "Who was Ferrari's founder?", //Enzo Ferrari
-                        "What was the first thing manufactured by Lamborghini?" //Tractor
+                "Which car company invented the modern-day seatbelt?", //Volvo
+                "What does SUV stand for?", //Sport Utility Vehicle
+                "What does ATV stand for?", //All Terrain Vehicle
+                "What is the highest-octane commercially available gas type in NL?", //91
+                "When was the first Front-Wheel-Drive car built?", //1909
+                "Which was the first car company to manufacture a 'Super Car'?", //Lamborghini
+                "Which mainstream car company is primarily known for their reliability?", //Honda or Toyota
+                "When was the first electric vehicle produced?", //1832
+                "What is the best selling car model in the world?", //Corolla
+                "When was the first speeding ticket issued?", //1902
+                "Who invented the term 'Tune-Up' as in to tune up your car?", //Henry Ford
+                "How many total car models contain factory cd-players now?", //3
+                "Which car brand was the first to incorporate LED running lights?", //Audi
+                "In an automatic vehicle, your drive selectors include P-R-N-D-L, what does the L stand for?", //Low
+                "Which car brand is known for its immensley small size?", //BMW
+                "Who was Ferrari's founder?", //Enzo Ferrari
+                "What was the first thing manufactured by Lamborghini?", //Tractor
+                "Which car brand's logo displays a 'Prancing Horse'?", //Ferrari
+                "Which sub-brand is known for its racing horse logo?", //Mustang
+                "What is the name of any South Korean car company?", //Kia or Hyundai
+                "Which car manufacturer is known for it premium luxury?", //Bentley or Rolls Royce
+                "Which sports car's design was heavily influenced by the Volkswagen Beetle?", //Porsche 911 or 911
+                "What is the name of the race series in which drivers only take left-turs?", //Nascar
+                "What is the name of the highly intense race series, using cutting edge tech, loved worldwide?", //F1 or Formula 1
+                "Which car brand logo is made up of three diamond shapes?", //Mitsubishi
+                "Which car brand is known for their flat-profile 'Boxer' engines?" //Subaru
                 ],
-
+    correctQuestions: [],
+    usedQuestions: [],
     currentQuestion: 0,
     answerCount: 0,
     correctCount: 0,
